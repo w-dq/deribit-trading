5aaa7083a79bf31d41f4218d8a978a8a3c05f3dd
backtohistory_533262-at-715158845633 <43607007+w-dq@users.noreply.github.com>
2023-03-30 00:49:43 +0800

    finish pull data

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # deribit-trading
+
+install : `asyncio`, `websockets` and `pandas`
+
+## Pull data
+
+### discover instruments
+
+Run `python pull_data.py -i` to retrieve all available trading instruments in `instruments.csv`.
+
+use `--currency` option to choose currency, default is `"BTC"`
+
+!!! different currentcy has different related products, e.g. `"USDC"` has no optionsonly perpetuals.
+
+```
+usage: pull_data.py [-h] [-i] [--currency CURRENCY]
+
+optional arguments:
+  -h, --help           show this help message and exit
+  -i, --instruments    select to get list of avaliable instruments
+  --currency CURRENCY  Choose counter currency: [BTC,ETH,USDC]
+```
+
+
+
+### get data
+
+Run `python pull_data.py` to get data, data is store under `data/` folder.
+
+Change the `pull_data.json` params to cunstomize the data collected
+
+```json
+{
+    "instrument_name" : "BTC-PERPETUAL",
+    "start_timestamp" : "2023-03-27 23:01:59",
+    "end_timestamp" : "now",
+    "resolution" : "5"
+}
+```
+
+Find the `instrument_name` from `instruments.csv` or https://test.deribit.com/api_console/
+
+Do not change the <u>format</u> of `start_timestamp`
+
+`end_timestamp` can be change to fix time, `now` is just an option
+
+Resolution is in minutes and has to be in: `["1","3","5","10","15","30","60","120","180","360","720","1D"]`, note that some data may not have `"1D"` available.
+
+
+
+**If start time is earlier than earliest date, system will still work, it will retrieve the <u>maximum data possible</u>,  just that the file name may not be that same as actual time range** 
+
+
+
+> **some notes**
+>
+> !!! BTC-PERPETUAL goes back to as far as 2018-08-13
+>
+> !!! BTC-29SEP23-28000-C goes back to as far as 2023-01-25
+
+
+
+833 - public/get_tradingview_chart_data
+834 - public/get_instruments 
---
+++ pull_data.py
@@ -0,0 +1,133 @@
+import asyncio
+import websockets
+import json
+from datetime import datetime
+from datetime import timedelta
+import argparse
+import pandas
+
+import pandas as pd
+
+base_wss = 'wss://test.deribit.com/ws/api/v2'
+
+
+def to_timestamp_in_milliseconds(time_string):
+    epoch = datetime.strptime(time_string, "%Y-%m-%d %H:%M:%S")
+    timestamp_in_milliseconds = int(epoch.timestamp() * 1000)
+    return timestamp_in_milliseconds
+
+def to_filename_timestr(stamp_in_milli):
+    dt_object = datetime.fromtimestamp(stamp_in_milli/1000)
+    dt_string = dt_object.strftime("%Y-%m-%d-%H:%M")
+    return dt_string
+
+def get_datetime_col(start,end,resolution):
+    if resolution == "1D":
+        resolution = 24*60
+    else:
+        resolution = int(resolution)
+    start_datetime = datetime.fromtimestamp(start/1000)
+    end_datetime = datetime.fromtimestamp(end/1000)
+    timedelta_resolution = timedelta(minutes=resolution)
+    datetime_strings = []
+    current_datetime = start_datetime
+    while current_datetime <= end_datetime:
+        datetime_strings.append(current_datetime.strftime("%Y-%m-%d %H:%M"))
+        current_datetime += timedelta_resolution
+    datetime_strings.append(current_datetime.strftime("%Y-%m-%d %H:%M"))
+    return datetime_strings
+
+
+async def call_instruments(msg):
+    async with websockets.connect(base_wss) as websocket:
+        await websocket.send(msg)
+        response = await websocket.recv()
+        response = json.loads(response)
+        # try:
+        data = response["result"]
+        df = pd.DataFrame(data)
+        df.insert(0, "instrument_name", df.pop("instrument_name"))
+        df.to_csv("instruments.csv", index=False)
+        # except:
+        #     print("error while dealing with response")
+
+async def call_chart_data(msg):
+    async with websockets.connect(base_wss) as websocket:
+        await websocket.send(msg)
+        response = await websocket.recv()
+        response = json.loads(response)
+        if "error" in response.keys():
+            print(response)
+            return _,"error"
+        else:
+            if response["result"]["status"] == "ok":
+                del response["result"]["status"]
+                df = pd.DataFrame(response["result"])
+                return df,"ok"
+            else:
+                return df,response["result"]["status"]
+        
+
+def get_instruments(currency):
+    msg = \
+    {
+        "jsonrpc" : "2.0",
+        "id" : 834,
+        "method" : "public/get_instruments",
+        "params" : {
+            "currency" : currency
+        }
+    }
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(call_instruments(json.dumps(msg)))
+
+def get_data(params,filename):
+    msg = \
+    {
+        "jsonrpc" : "2.0",
+        "id" : 833,
+        "method" : "public/get_tradingview_chart_data",
+        "params" : params
+    }
+    print(msg)
+    loop = asyncio.get_event_loop()
+    result_df, status = loop.run_until_complete(call_chart_data(json.dumps(msg)))
+    if status == "ok":
+        time_col = get_datetime_col(params["start_timestamp"],params["end_timestamp"],params["resolution"])
+        result_df.insert(0,"datetime",time_col[-result_df.shape[0]:])
+        result_df.to_csv("data/"+filename+".csv", index=False)
+    elif status == "no_data":
+        print("no data for this set of config! check instrument, time and resolution")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--instruments", action="store_true", help="select to get list of avaliable instruments")
+    parser.add_argument("--currency", required=False, default='BTC',
+                         help=('Choose related currency: [BTC,ETH,USDC]'))
+    args = parser.parse_args()
+    if args.instruments:
+        # TODO: add kind param
+        c = args.currency
+        if c not in ["BTC","ETH","USDC"]:
+            c = "BTC"
+        get_instruments(c)
+    else:
+        with open("pull_data.json", "r") as f:
+            params = json.load(f)
+        params["start_timestamp"] = to_timestamp_in_milliseconds(params["start_timestamp"])
+        if params["end_timestamp"] == "now":
+            params["end_timestamp"] = int(datetime.now().timestamp() * 1000)
+        else:
+            params["end_timestamp"] = to_timestamp_in_milliseconds(params["end_timestamp"])
+        if params["resolution"] in ["1","3","5","10","15","30","60","120","180","360","720","1D"]:
+            filename = "_".join([params["instrument_name"],
+                                 to_filename_timestr(params["start_timestamp"]),
+                                 to_filename_timestr(params["end_timestamp"]),
+                                 params["resolution"]
+                                ])
+            print(filename)
+            get_data(params,filename)
+        else:
+            print("check resolution")
