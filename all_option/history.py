941a77a3c8b57fd68c4cd537a8d15cf90d897f4a
backtohistory_533262-at-715158845633 <43607007+w-dq@users.noreply.github.com>
2023-11-20 22:00:22 +0800

    backup

--- all_option/history.py
+++ all_option/history.py
@@ -8,6 +8,7 @@ import argparse
 import pandas
 from tqdm import tqdm
 
+import csv
 import pandas as pd
 
 import requests
@@ -17,9 +18,9 @@ ins_url = 'https://history.deribit.com/api/v2/public/get_instruments'
 
 base_url = 'https://history.deribit.com/api/v2/public/get_last_trades_by_instrument'
 
-def run_analysis(name_list):
+def run_analysis(name_list,f):
     res = []
-    for ins_name in tqdm(name_list):
+    for ins_name in tqdm(name_list[46000:]):
         params = {
             "count": 10000,
             "include_old": True,
@@ -29,6 +30,7 @@ def run_analysis(name_list):
         if response.status_code == 200:
             data = json.loads(response.content)
             num_of_trades = len(data["result"]["trades"])
+            # print(num_of_trades)
             if num_of_trades > 10:
                 first_trade = data["result"]["trades"][-1]
                 last_trade = data["result"]["trades"][0]
@@ -38,7 +40,7 @@ def run_analysis(name_list):
                 l_dt_object = datetime.fromtimestamp(last_trade["timestamp"]/1000)
                 l_dt_string = l_dt_object.strftime("%Y-%m-%d %H:%M:%S")
                 time_diff = l_dt_object - f_dt_object
-                
+                # print(str(time_diff),time_diff.days)
                 if time_diff.days > 1:
                     row = { "instrument_name":ins_name,
                             "number_of _trades":num_of_trades,
@@ -46,6 +48,8 @@ def run_analysis(name_list):
                             "end_time":l_dt_string,
                             "days":time_diff.days}
                     res.append(row)
+                    f.writerow(row.values())
+
         else:
             print(ins_name,"error")
         
@@ -58,11 +62,12 @@ if __name__ == "__main__":
 
     tag = True
     if args.instruments:
-        response = requests.get("https://history.deribit.com/api/v2/public/get_instruments?currency=BTC&include_old=true")
+        response = requests.get(ins_url + "?currency=BTC&expired=true")
         if response.status_code == 200:
             data = json.loads(response.content)["result"]
             df = pd.DataFrame(data)
             df_filtered = df.loc[df['kind'] == 'option']
+            df_filtered = df_filtered.loc[df_filtered['expiration_timestamp'] < datetime.now().timestamp()*1000]
             df_filtered.to_csv("expired_btc.csv")
             print("BTC data loaded successfully.")
         else:
@@ -80,11 +85,21 @@ if __name__ == "__main__":
         #     tag = False
         #     print("Failed to load ETH data.")
     
-    # if tag:
-    #     instrument_name = pd.read_csv("expired_btc.csv")
-    #     time_data = run_analysis(instrument_name["instrument_name"])
-    #     df = pd.DataFrame(time_data)
-    #     df.to_csv("expired_btc_time_summary.csv")
+    if tag:
+        instrument_name = pd.read_csv("expired_btc.csv")
+        
+        with open("expired_btc_time_summary.csv", 'a') as f:
+            writer = csv.writer(f)
+            # writer.writerow(["instrument_name",
+            #                 "number_of_trades",
+            #                 "start_time",
+            #                 "end_time",
+            #                 "days"])
+            time_data = run_analysis(instrument_name["instrument_name"],writer)
+            df = pd.DataFrame(time_data)
+        
+
+        df.to_csv("new.csv")
 
         # instrument_name = pd.read_csv("expired_eth.csv")
         # time_data = run_analysis(instrument_name["instrument_name"])
--- strategy/__init__.py
+++ strategy/__init__.py
@@ -1,3 +1,22 @@
 
 from .DualThrust import DualThrust
 from .TestStrategy import TestStrategy
+
+import logging
+
+class BaseStrategy():
+    def __init__(self):
+        self.logger = logging.getLogger("BASE")
+    
+    def on_data(self,data):
+        self.logger.INFO("New data point recieved")
+        return None
+
+    def get_subscribe_channles(self):
+        return ["chart.trades.BTC-PERPETUAL.1"]
+
+    def confirm_msg(self,msg):
+        self.logger.INFO("Confirm on order placed")
+        
+    
+
